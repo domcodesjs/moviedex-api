ad013e9b9b3b1a0469e6166e11c72ba97aa6d0b2
Dominic Faulring <domcodesjs@gmail.com>
2020-10-09 17:11:56 -0700

    added a helmet

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
